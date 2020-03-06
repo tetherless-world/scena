e5302732d3387ca80213eebb0caeba887b04b289
Minor Gordon <gordom6@rpi.edu>
2020-03-06 10:44:54 -0500

    move various *Properties mixins over

---
+++ src/main/scala/io/github/tetherlessworld/scena/RdfProperties.scala
@@ -0,0 +1,29 @@
+package io.github.tetherlessworld.scena
+
+import org.apache.jena.rdf.model.Resource
+import org.apache.jena.vocabulary.RDF
+
+trait RdfProperties extends PropertyGetters with PropertySetters {
+  /**
+   * Get the first rdf:type of a resource.
+   */
+  final def `type` = types.headOption
+
+  /**
+   * Get all rdf:type's of a resource.
+   */
+  final def types = getPropertyObjectResources(RDF.`type`)
+
+  /**
+   * Set the rdf:type of of a resource, clearing out other types.
+   */
+  final def type_=(typeResource: Resource) =
+    this.types_=(List(typeResource))
+
+  /**
+   * Set the rdf:types of a resource, clearing out other types.
+   */
+  final def types_=(typeResources: List[Resource]): Unit = {
+    setProperty(RDF.`type`, typeResources)
+  }
+}
